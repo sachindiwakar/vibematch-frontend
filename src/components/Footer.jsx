5e7f590b2dbb33ca447999abdc3dfb9694c515d7
Sachin Diwakar <sachindiwakarp98@gmail.com>
2026-05-16 00:52:32 +0530

    add navbar footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { Link } from "react-router";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-base-300 text-neutral-content p-4">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-4 mx-auto">
+        <p className="text-sm text-center md:text-left">
+          © {new Date().getFullYear()} VibeMatch. All rights reserved.
+        </p>
+
+        <div className="flex flex-wrap justify-center gap-4 text-sm">
+          <Link to="/about" className="hover:underline hover:text-white">
+            About
+          </Link>
+
+          <Link to="/contact" className="hover:underline hover:text-white">
+            Contact
+          </Link>
+
+          <Link
+            to="/privacy-policy"
+            className="hover:underline hover:text-white"
+          >
+            Privacy Policy
+          </Link>
+
+          <Link
+            to="/refund-policy"
+            className="hover:underline hover:text-white"
+          >
+            Refund Policy
+          </Link>
+
+          <Link
+            to="/terms-and-conditions"
+            className="hover:underline hover:text-white"
+          >
+            Terms & Conditions
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router";
+import { BASE_URL } from "../utils/constants";
+import { removeUser } from "../store/slices/userSlice";
+import { clearFeed } from "../store/slices/feedSlice";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const user = useSelector((state) => state.user);
+  const request = useSelector((state) => state.request);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(
+        BASE_URL + "/auth/logout",
+        {},
+        { withCredentials: true },
+      );
+      dispatch(removeUser());
+      dispatch(clearFeed());
+      navigate("/login");
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <div className="navbar bg-base-300 shadow-sm">
+      <div className="flex-1">
+        <Link
+          to="/"
+          className="bg-gray-100 text-black text-lg md:text-xl font-bold py-2 px-2 rounded-xl m-2 whitespace-nowrap"
+        >
+          💕Vibe Match
+        </Link>
+      </div>
+
+      <div className="flex gap-2">
+        {user && (
+          <div className="flex items-center gap-3 mx-5">
+            <p className="p-2">Welcome, {user.firstName}!</p>
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img alt="user-profile-photo" src={user.photoUrl} />
+                </div>
+              </div>
+
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 w-52 rounded-box bg-base-100 shadow z-1"
+              >
+                <li>
+                  <Link to="/profile" className="justify-between">
+                    Profile
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/connections">Connections</Link>
+                </li>
+                <li>
+                  <Link to="/requests">
+                    Requests{" "}
+                    {request.length > 0 && (
+                      <span className="badge">Pending</span>
+                    )}
+                  </Link>
+                </li>
+                <li>
+                  <a onClick={handleLogout}>Logout</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
