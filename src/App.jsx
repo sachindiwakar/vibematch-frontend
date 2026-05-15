fdbcbdf148d895bad596cc19966a06a561ea3fb6
Sachin Diwakar <sachindiwakarp98@gmail.com>
2026-05-16 00:08:23 +0530

    add redux store slices

--- src/App.jsx
+++ src/App.jsx
@@ -16,35 +16,42 @@ import PrivacyPolicy from "./pages/PrivacyPolicy";
 import RefundPolicy from "./pages/RefundPolicy";
 import TermsConditions from "./pages/TermsConditions";
 import NotFound from "./pages/NotFound";
+import { Provider } from "react-redux";
+import appStore from "./store/appStore";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/" element={<Feed />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/connections" element={<Connections />} />
-            <Route path="/requests" element={<Requests />} />
-            <Route path="/chat/:targetUserId" element={<Chat />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-            <Route path="/verify" element={<Verify />} />
-            <Route
-              path="/profile/verify/delete-account"
-              element={<DeleteAccount />}
-            />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/refund-policy" element={<RefundPolicy />} />
-            <Route path="/terms-and-conditions" element={<TermsConditions />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+              <Route path="/chat/:targetUserId" element={<Chat />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
+              <Route path="/verify" element={<Verify />} />
+              <Route
+                path="/profile/verify/delete-account"
+                element={<DeleteAccount />}
+              />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="/refund-policy" element={<RefundPolicy />} />
+              <Route
+                path="/terms-and-conditions"
+                element={<TermsConditions />}
+              />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
---
+++ src/store/appStore.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./slices/userSlice";
+import feedReducer from "./slices/feedSlice";
+import connectionReducer from "./slices/connectionSlice";
+import requestReducer from "./slices/requestSlice";
+export const appStore = configureStore({
+  reducer: {
+    user: userReducer,
+    feed: feedReducer,
+    connection: connectionReducer,
+    request: requestReducer,
+  },
+});
+
+export default appStore;
